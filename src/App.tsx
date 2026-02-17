0d30b66e85b9ac5fca51abc5e47ee526a2162ffa
kenjioosawa09 <kenjihima@gmail.com>
2026-02-17 18:17:52 +0900

    開発中かつ、スマホで結果が確認できないため、URLのリクエスト先を brouserRouter→HashRouterに変更。

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import {useState } from 'react';
-import{BrowserRouter, Routes, Route, Navigate} from 'react-router-dom';
+import{HashRouter, Routes, Route, Navigate} from 'react-router-dom'; 
+//HashRouterは練習用なので、本番環境にした場合はBrowserRouterに変更する必要あり
 import{LoginForm} from './types';
 import {LoginPage} from './LoginPage';
 import {DashboardPage} from './DashboardPage';
 
 export default function App(){
     const[loggedInUser, setLoggedInUser] = useState<LoginForm | null>(()=>{
+     try{
       const saved = localStorage.getItem("user");
       return saved ? JSON.parse(saved) : null;
-    });
+    }
+    //データの読み込みに失敗した場合のエラーハンドリング
+    catch(e){
+      console.error("データの読み込みに失敗しました");
+      return null;
+    }
+  });
 
     const handleLogin =(user: LoginForm) => {
       setLoggedInUser(user);
@@ -21,7 +29,7 @@ export default function App(){
     };
 
     return(
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Navigate to="/login" replace/>}/> 
           <Route path="/login" element=
@@ -32,7 +40,7 @@ export default function App(){
           }
         />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     );
 }
 
